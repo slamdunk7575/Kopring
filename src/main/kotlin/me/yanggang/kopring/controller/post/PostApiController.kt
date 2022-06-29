c3d25e785143386144ac4eb35a3204ba46914302
slamdunk7575 <slamdunk7575@gmail.com>
2022-06-29 21:20:37 +0900

    (feat) POST (REST-API)

---
+++ src/main/kotlin/me/yanggang/kopring/controller/post/PostApiController.kt
@@ -0,0 +1,16 @@
+package me.yanggang.kopring.controller.post
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PostApiController {
+
+    @PostMapping("/post-mapping")
+    fun postMapping(): String {
+        return "post-mapping"
+    }
+
+}
