d14a2b60dad5ea53fab22c8b494c9853fdc93162
slamdunk7575 <slamdunk7575@gmail.com>
2022-06-30 20:54:49 +0900

    (feat) Response Entity (DELETE에 대한 응답)

--- src/main/kotlin/me/yanggang/kopring/controller/response/ResponseApiController.kt
+++ src/main/kotlin/me/yanggang/kopring/controller/response/ResponseApiController.kt
@@ -3,7 +3,9 @@ package me.yanggang.kopring.controller.response
 import me.yanggang.kopring.model.http.UserRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -40,5 +42,11 @@ class ResponseApiController {
     fun putMapping(@RequestBody userRequest: UserRequest?): ResponseEntity<UserRequest> {
         // (가정) 기존 데이터가 없어서 새로 생성함
         return ResponseEntity.status(HttpStatus.CREATED).body(userRequest)
+    }
+
+    // 4. DELETE (500)
+    @DeleteMapping("/{id}")
+    fun deleteMapping(@PathVariable id: Int): ResponseEntity<Nothing> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)
     }
 }
