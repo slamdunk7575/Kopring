508a642334d3dccec345125fe924e254ff9a5988
slamdunk7575 <slamdunk7575@gmail.com>
2022-06-30 19:55:34 +0900

    (feat) Response Entity (GET에 대한 응답)

---
+++ src/main/kotlin/me/yanggang/kopring/controller/response/ResponseApiController.kt
@@ -0,0 +1,25 @@
+package me.yanggang.kopring.controller.response
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/response")
+class ResponseApiController {
+
+    @GetMapping("")
+    fun getMapping(@RequestParam age: Int?): ResponseEntity<String> {
+        return age?.let {
+            if (age < 20) {
+                return ResponseEntity.status(400).body("age 값은 20 보다 커야 합니다.")
+            }
+            ResponseEntity.ok("OK")
+        }?: kotlin.run {
+            return ResponseEntity.status(400).body("age 값이 누락되었습니다.")
+        }
+    }
+
+}
