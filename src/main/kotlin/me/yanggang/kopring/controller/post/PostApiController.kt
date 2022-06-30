6679aebdabb69901379736d119b6f2c0cbd24da7
slamdunk7575 <slamdunk7575@gmail.com>
2022-06-30 12:32:00 +0900

    (feat) Request Body 추가 (POST)

--- src/main/kotlin/me/yanggang/kopring/controller/post/PostApiController.kt
+++ src/main/kotlin/me/yanggang/kopring/controller/post/PostApiController.kt
@@ -1,9 +1,7 @@
 package me.yanggang.kopring.controller.post
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import me.yanggang.kopring.model.http.UserRequest
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
@@ -17,6 +15,15 @@ class PostApiController {
     @RequestMapping(method = [RequestMethod.POST], path = ["/request-mapping"])
     fun requestMapping(): String {
         return "request-mapping"
+    }
+
+    // 스프링에서 JSON 데이터와 Object를 주고 받을때 Object Mapper 사용
+    // JSON -> Object
+    // Object -> JSON
+    @PostMapping("/post-mapping/object")
+    fun postMappingObject(@RequestBody userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
     }
 
 }
