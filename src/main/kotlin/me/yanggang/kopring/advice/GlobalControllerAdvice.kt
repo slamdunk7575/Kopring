cb76fce31083ed7b4d92bcacd0c3ff76042e8047
slamdunk7575 <slamdunk7575@gmail.com>
2022-07-05 14:54:33 +0900

    (feat) RequestParam 예외시 응답 처리

--- src/main/kotlin/me/yanggang/kopring/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/me/yanggang/kopring/advice/GlobalControllerAdvice.kt
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class]) // 특정 Controller 에 대한 예외만 처리하도록 지정할 수도 있음
+// @RestControllerAdvice(basePackageClasses = [ExceptionApiController::class]) // 특정 Controller 에 대한 예외만 처리하도록 지정할 수도 있음
 class GlobalControllerAdvice {
 
     @ExceptionHandler(value = [RuntimeException::class])
--- src/main/kotlin/me/yanggang/kopring/exception/ExceptionApiController.kt
+++ src/main/kotlin/me/yanggang/kopring/exception/ExceptionApiController.kt
@@ -1,14 +1,26 @@
 package me.yanggang.kopring.exception
 
+import me.yanggang.kopring.model.http.Error
+import me.yanggang.kopring.model.http.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import javax.servlet.http.HttpServletRequest
+import javax.validation.ConstraintViolation
+import javax.validation.ConstraintViolationException
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 @RestController
 @RequestMapping("/api/exception")
+@Validated
 class ExceptionApiController {
 
     @GetMapping("/hello")
@@ -16,6 +28,49 @@ class ExceptionApiController {
         // throw RuntimeException("강제 Exception 발생")
         val list = mutableListOf<String>()
         val temp = list[0]
+    }
+
+    @GetMapping("")
+    fun get(
+        @NotBlank
+        @Size(min = 2, max = 6)
+        @RequestParam name: String,
+
+        @Min(10)
+        @RequestParam age: Int
+    ): String {
+        println(name)
+        println(age)
+        return "$name $age"
+    }
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun constraintViolationException(e: ConstraintViolationException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        // 에러 분석
+        val errors = mutableListOf<Error>()
+
+        e.constraintViolations.forEach {
+            var error = Error().apply {
+                this.field = it.propertyPath.last().name
+                this.message = it.message
+                this.value = it.invalidValue
+            }
+
+            errors.add(error)
+        }
+
+        // ErrorResponse
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
+            this.message = "요청에 에러가 발생하였습니다"
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors = errors
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
     }
 
     // 클래스 내부에 ExceptionHandler 정의하면 해당 Controller 내부의 예외만 처리할 수 있음 (GlobalControllerAdvice 타지 않음)
