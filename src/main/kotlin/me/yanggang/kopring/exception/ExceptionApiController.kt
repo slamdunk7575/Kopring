6c0a32e360390e9a15a0f4ca29a0f29976357801
slamdunk7575 <slamdunk7575@gmail.com>
2022-07-05 15:15:19 +0900

    (feat) Bean Validation 예외시 응답 처리

--- src/main/kotlin/me/yanggang/kopring/exception/ExceptionApiController.kt
+++ src/main/kotlin/me/yanggang/kopring/exception/ExceptionApiController.kt
@@ -2,11 +2,16 @@ package me.yanggang.kopring.exception
 
 import me.yanggang.kopring.model.http.Error
 import me.yanggang.kopring.model.http.ErrorResponse
+import me.yanggang.kopring.model.http.UserRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +19,7 @@ import java.time.LocalDateTime
 import javax.servlet.http.HttpServletRequest
 import javax.validation.ConstraintViolation
 import javax.validation.ConstraintViolationException
+import javax.validation.Valid
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
@@ -43,6 +49,43 @@ class ExceptionApiController {
         println(age)
         return "$name $age"
     }
+
+
+    @PostMapping
+    fun post(@Valid @RequestBody userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+
+
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun MethodArgumentNotValidException(e: MethodArgumentNotValidException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        val errors = mutableListOf<Error>()
+
+        e.bindingResult.allErrors.forEach { errorObject ->
+            val error = Error().apply {
+                this.field = (errorObject as FieldError).field
+                this.message = errorObject.defaultMessage
+                this.value = errorObject.rejectedValue
+            }
+            errors.add(error)
+        }
+
+        // ErrorResponse
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
+            this.message = "요청에 에러가 발생하였습니다"
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors = errors
+        }
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+    }
+
 
     @ExceptionHandler(value = [ConstraintViolationException::class])
     fun constraintViolationException(e: ConstraintViolationException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
